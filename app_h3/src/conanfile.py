112be532d9d40d233466a6b63ef006d3c2760704
prrathnayake <pasanransika1@gmail.com>
2023-04-19 00:28:20 +0530

    change base recipe

--- app_h3/src/conanfile.py
+++ app_h3/src/conanfile.py
@@ -2,11 +2,11 @@ from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
 
 class H3Recipe(ConanFile):
-    python_requires = "cpp-tools/1.0"
-    python_requires_extend = "cpp-tools.baseRecipe"
+    python_requires = "cpp-tools/1.0@pasan/testing"
+    python_requires_extend = "cpp-tools.BaseRecipe"
 
     def init(self):
-        base = self.python_requires["cpp-tools"].module.baseRecipe
+        base = self.python_requires["cpp-tools"].module.BaseRecipe
         base.name = "app_h3"
         base.version = "1.0"
         base.exports_sources = "CMakeLists.txt", "h3/*"
--- app_utils/src/conanfile.py
+++ app_utils/src/conanfile.py
@@ -2,11 +2,11 @@ from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 
 class kafkaRecipe(ConanFile):
-    python_requires = "cpp-tools/1.0"
-    python_requires_extend = "cpp-tools.baseRecipe"
+    python_requires = "cpp-tools/1.0@pasan/testing"
+    python_requires_extend = "cpp-tools.BaseRecipe"
 
     def init(self):
-        base = self.python_requires["cpp-tools"].module.baseRecipe
+        base = self.python_requires["cpp-tools"].module.BaseRecipe
         base.name = "app_utils"
         base.version = "1.0"
         base.exports_sources = "CMakeLists.txt", "utils/*"
