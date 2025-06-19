db0b7273d466df7beea32301da15c82dac8ffa29
prrathnayake <pasanransika1@gmail.com>
2025-06-20 01:37:29 +1000

    update file logger

--- app_utils/src/utils/log/fileLogger.cpp
+++ app_utils/src/utils/log/fileLogger.cpp
@@ -1,19 +1,89 @@
 #include <iostream>
 #include <fstream>
+#include <fcntl.h>
+#include <unistd.h>
+#include <filesystem>
+#include <mutex>
 
-#include "../index.h"
+#include "../index.h" // or wherever FileLogger is declared
 
-utils::FileLogger::FileLogger()
+namespace fs = std::filesystem;
+std::mutex utils::FileLogger::logMutex;
+
+utils::FileLogger::FileLogger(const std::string& path)
+    : logFilePath(path)
 {
-    logFile.open("log.txt", std::ios::out);
+    fs::path logPath(logFilePath);
+    if (fs::exists(logFilePath))
+    {
+        fs::remove(logFilePath);
+    }
+
+    if (logPath.has_parent_path())
+    {
+        fs::create_directories(logPath.parent_path());
+    }
 }
 
-utils::FileLogger::~FileLogger()
+utils::FileLogger::~FileLogger() {}
+
+void utils::FileLogger::log(MessageCode level, const std::string &message)
 {
-    logFile.close();
+    print(levelToString(level), message);
 }
 
-void utils::FileLogger::info(std::string message)
+void utils::FileLogger::logMeta(MessageCode level, const std::string &message,
+                                const char *file, int line, const char *func)
 {
-    logFile << utils::Time::logTime() << " [INFO] " << message << "\n";
+    std::string metadata = " => " + std::string(file) + ":" + std::to_string(line) +
+                           " in " + func;
+    std::string full_message = utils::Time::logTime() + " [" + levelToString(level) + "] " +
+                               metadata + " | " + message;
+    print(levelToString(level), full_message);
+}
+
+void utils::FileLogger::print(const std::string &levelStr, const std::string &message)
+{
+    std::lock_guard<std::mutex> lock(logMutex); // thread-safe
+
+    int fd = open(logFilePath.c_str(), O_WRONLY | O_CREAT | O_APPEND, 0644);
+    if (fd == -1)
+    {
+        perror("open");
+        return;
+    }
+
+    struct flock fl{};
+    fl.l_type = F_WRLCK;
+    fl.l_whence = SEEK_SET;
+
+    fcntl(fd, F_SETLKW, &fl); // lock the file
+
+    std::string full_message = utils::Time::logTime() + " [" + levelStr + "] " + message + "\n";
+    if (write(fd, full_message.c_str(), full_message.size()) == -1)
+    {
+        perror("write");
+    }
+
+    fl.l_type = F_UNLCK;
+    fcntl(fd, F_SETLK, &fl); // unlock
+
+    close(fd);
+}
+
+std::string utils::FileLogger::levelToString(MessageCode level)
+{
+    switch (level)
+    {
+    case INFO:
+        return "INFO";
+    case ERROR:
+        return "ERROR";
+    case WARN:
+        return "WARN";
+    case DEBUG:
+        return "DEBUG";
+    default:
+        return "UNKNOWN";
+    }
 }
--- app_utils/src/utils/log/fileLogger.h
+++ app_utils/src/utils/log/fileLogger.h
@@ -1,14 +1,35 @@
-#pragma one
-#include <iostream>
-#include <fstream>
+#pragma once
 
-namespace utils{
-    class FileLogger: public Logger{
-        private:
-            std::fstream logFile;
-        public:
-            FileLogger();
-            ~FileLogger();
-            void info(std::string message);
+#include <string>
+#include <mutex>
+
+namespace utils
+{
+
+    class FileLogger
+    {
+    public:
+        explicit FileLogger(const std::string& logFilePath = "log/log.txt");
+        ~FileLogger();
+        enum MessageCode
+        {
+            INFO = 0,
+            ERROR = 1,
+            WARN = 2,
+            DEBUG = 3
+        };
+        void log(MessageCode level, const std::string &message);
+        void logMeta(MessageCode level, const std::string &message,
+                     const char *file, int line, const char *func);
+
+    private:
+        void print(const std::string &level, const std::string &message);
+        static std::string levelToString(MessageCode level);
+        std::string logFilePath; 
+        static std::mutex logMutex;
     };
-}
+
+} // namespace utils
+
+#define LOG(logger, level, msg) \
+    (logger).log((level), (msg), __FILE__, __LINE__, __func__)
