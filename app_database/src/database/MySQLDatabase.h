0d06493bb7b42c04e96671bd132d5bbd8b764102
prrathnayake <pasanransika1@gmail.com>
2025-06-23 20:26:56 +1000

    fix sql script running process

--- app_database/src/database/MySQLDatabase.h
+++ app_database/src/database/MySQLDatabase.h
@@ -1,19 +1,32 @@
 #pragma once
-#include <mysql/mysql.h>
-#include <iostream>
+#include <string>
+
+struct MYSQL;
 
 namespace database
 {
+
     class MySQLDatabase
     {
     public:
         MySQLDatabase();
         ~MySQLDatabase();
 
-        bool connect(const std::string &host, const std::string &user, const std::string &password, const std::string &db, unsigned int port);
+        bool connect(const std::string &host, const std::string &user,
+                     const std::string &password, const std::string &db, unsigned int port);
+
+        bool isConnected() const;
+        void disconnect();
+
+        bool executeUpdate(const std::string &query); // INSERT, UPDATE, DELETE
+        bool executeQuery(const std::string &query);  // SELECT
+        bool initializeDatabase(const std::string &dbName);
+        bool runSqlScript(const std::string &filePath);
+
+        std::string escapeString(const std::string &input);
 
     private:
         MYSQL *conn;
     };
 
-}
+};
